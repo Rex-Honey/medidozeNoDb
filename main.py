609f46b94bec81b4ad70ebf2d0c56eb14c462b5a
rex <er.rexhoney@gmail.com>
2025-07-08 12:05:38 +0530

    Implement server configuration handling in MainWindow, including JSON config loading and connection setup. Add error handling for missing config and integrate ServerConfigWindow for user input. Update main window initialization to include QSS stylesheet.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QFrame, QGridLayout, QStackedLayout
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QStandardPaths
 from PyQt6.QtGui import QFont, QIcon
-import sys, os
+import json, pyodbc, sys, os
 from pages.sigin import SignInWindow
+from pages.serverConfig import ServerConfigWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,20 +14,51 @@ class MainWindow(QMainWindow):
         module_dir = os.path.dirname(__file__)
         self.setWindowTitle("Medidoze Technologies Inc.")
         self.setWindowIcon(QIcon(os.path.join(module_dir, 'images', 'medidoze-icon.ico')))
-        self.setStyleSheet("background-color: lightblue;")
+        self.setObjectName("mainWindow")
+
         self.setGeometry(250, 100, 1400, 830)
         self.setFixedSize(1400, 830)
 
-        self.signInWindow = SignInWindow()
-        self.signInWindow.loginSuccess.connect(self.loginSuccess)
         self.stackLayout = QStackedLayout()
-        self.stackLayout.addWidget(self.signInWindow)
-        
         centralWidget = QWidget()
         centralWidget.setLayout(self.stackLayout)
         self.setCentralWidget(centralWidget)
 
-        self.createTables()
+        self.signInWindow = SignInWindow()
+        self.signInWindow.loginSuccess.connect(self.loginSuccess)
+        self.stackLayout.addWidget(self.signInWindow)
+        self.checkConfig()
+
+    def checkConfig(self):
+        try:
+            documentsDir = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.DocumentsLocation)
+            with open(os.path.join(documentsDir, 'medidoze', 'config2.json'), 'r', encoding='utf-8') as f:
+                config = json.load(f)
+            connString = (
+                f"DRIVER={{SQL Server}};"
+                f"SERVER={config['server']};"
+                f"DATABASE={config['local_database']};"
+                f"UID={config['username']};"
+                f"PWD={config['password']};"
+            )
+            conn = pyodbc.connect(connString)
+            print("Connection successful!")
+            conn.close()
+        except FileNotFoundError:
+            print("No JSON config file found.")
+            self.serverConfigWindow = ServerConfigWindow()
+            self.stackLayout.addWidget(self.serverConfigWindow)
+            self.serverConfigWindow.serverSetUpDone.connect(self.serverSetUpDone)
+            self.stackLayout.setCurrentWidget(self.serverConfigWindow)
+        except Exception as e:
+            print(e)
+            self.serverConfigWindow = ServerConfigWindow()
+            self.stackLayout.addWidget(self.serverConfigWindow)
+            self.serverConfigWindow.serverSetUpDone.connect(self.serverSetUpDone)
+            self.stackLayout.setCurrentWidget(self.serverConfigWindow)
+
+    def serverSetUpDone(self, config, username):
+        self.stackLayout.setCurrentWidget(self.signInWindow)
 
     def loginSuccess(self, username, password):
         print(f"Login successful: {username}, {password}")
@@ -34,5 +66,8 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
+    qssPath = os.path.join(os.path.dirname(__file__), 'qss', 'style.qss')
+    with open(qssPath, 'r') as f:
+        app.setStyleSheet(f.read())
     mainWindow.show()
     sys.exit(app.exec())
