e5076ace72f58d9a193377d32a671a6bf95f688e
rexhoney <er.rexhoney@gmail.com>
2025-09-10 11:06:16 +0530

    Refactor PharmacyUsersWindow for improved code consistency and readability
    - Renamed functions and variables to follow camelCase convention for better clarity.
    - Updated image handling and user data management to enhance maintainability.
    - Refactored user interface interactions for improved user experience and consistency across the application.

--- pages/pharmacyUsers.py
+++ pages/pharmacyUsers.py
@@ -9,7 +9,7 @@ from functools import partial
 import base64
 
 
-def get_rounded_pixmap(pixmap, size):
+def getRoundedPixmap(pixmap, size):
     rounded = QPixmap(size, size)
     rounded.fill(Qt.GlobalColor.transparent)
     painter = QPainter(rounded)
@@ -27,11 +27,11 @@ class PharmacyUsersWindow(QWidget):
         self.config = config
         self.connString = connString
         self.userData = userData
-        self.local_conn = pyodbc.connect(connString)
+        self.localConn = pyodbc.connect(connString)
         self.rootDir = os.path.dirname(os.path.dirname(__file__))
-        ui_path = os.path.join(self.rootDir, "uiFiles", "pharmacyUsers.ui")
-        uic.loadUi(ui_path, self)
-        self.fetch_all_users()
+        uiPath = os.path.join(self.rootDir, "uiFiles", "pharmacyUsers.ui")
+        uic.loadUi(uiPath, self)
+        self.fetchAllUsers()
         self.btnAddUser.clicked.connect(self.openAddUserPage)
 
     def openAddUserPage(self):
@@ -40,8 +40,8 @@ class PharmacyUsersWindow(QWidget):
             from pages.pageContainer import PageContainer
 
             # Create the add user page
-            add_user_widget = AddUserWindow(self.config, self.connString,self.userData)
-            add_user_page = PageContainer("Add User", add_user_widget)
+            addUserWidget = AddUserWindow(self.config, self.connString,self.userData)
+            addUserPage = PageContainer("Add User", addUserWidget)
 
             # Find the main app's stack (walk up the parent chain)
             parent = self.parentWidget()
@@ -50,33 +50,33 @@ class PharmacyUsersWindow(QWidget):
                     break
                 parent = parent.parentWidget()
             if parent is not None:
-                parent.stack.addWidget(add_user_page)
-                parent.stack.setCurrentWidget(add_user_page)
+                parent.stack.addWidget(addUserPage)
+                parent.stack.setCurrentWidget(addUserPage)
             else:
                 print("Main stack not found!")
         except Exception as e:
             print(e)
 
-    def delete_user(self,username):
+    def deleteUser(self,username):
         try:
             reply = QMessageBox.information(None, 'Medidoze alert',
             f"Do you want to delete user '{username}' ?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No, QMessageBox.StandardButton.No)
 
             if reply == QMessageBox.StandardButton.Yes:
-                local_cursor = self.local_conn.cursor()
-                local_cursor.execute(f"UPDATE users SET isSoftDlt = 'Y' WHERE uid='{username}'")
-                self.local_conn.commit()
+                localCursor = self.localConn.cursor()
+                localCursor.execute(f"UPDATE users SET isSoftDlt = 'Y' WHERE uid='{username}'")
+                self.localConn.commit()
                 self.infoViewUsers.setText("User deleted successfully!!")
                 self.infoViewUsers.setStyleSheet("background:lightgreen;color:green;padding:12px;border-radius:none")
-                QTimer.singleShot(4000, self.clear_info_messages)
-                self.fetch_all_users()
+                QTimer.singleShot(4000, self.clearInfoMessages)
+                self.fetchAllUsers()
         except Exception as e:
             print(e)
 
     def addDataToUserTable(self,data):
         try:
             self.table_view_user.setRowCount(len(data))
-            for row,row_data in enumerate(data):
+            for row,rowData in enumerate(data):
                 
                 # ------------ image ------------
                 col=0
@@ -85,36 +85,36 @@ class PharmacyUsersWindow(QWidget):
                 size = 30
                 self.lblImg.setFixedSize(size, size)
                 self.lblImg.setScaledContents(True)
-                if row_data['image']:
-                    binary_data = base64.b64decode(row_data['image'])
-                    image = QImage.fromData(binary_data)
+                if rowData['image']:
+                    binaryData = base64.b64decode(rowData['image'])
+                    image = QImage.fromData(binaryData)
                     pixmap = QPixmap.fromImage(image).scaled(
                         30, 30, Qt.AspectRatioMode.KeepAspectRatioByExpanding, Qt.TransformationMode.SmoothTransformation
                     )
                 else:
-                    default_image_path = os.path.join(self.rootDir, "images", "user.jpg")
-                    image = QImage(default_image_path)
+                    defaultImagePath = os.path.join(self.rootDir, "images", "user.jpg")
+                    image = QImage(defaultImagePath)
                     pixmap = QPixmap.fromImage(image).scaled(
                         30, 30, Qt.AspectRatioMode.KeepAspectRatioByExpanding, Qt.TransformationMode.SmoothTransformation
                     )
 
                 # Make the pixmap round and set it
-                rounded_pixmap = get_rounded_pixmap(pixmap, size)
-                self.lblImg.setPixmap(rounded_pixmap)
+                roundedPixmap = getRoundedPixmap(pixmap, size)
+                self.lblImg.setPixmap(roundedPixmap)
                 self.table_view_user.setCellWidget(row, col, self.lblImg)
                 self.lblImg.setStyleSheet("padding-left:5px;border-radius:0px;background-color:transparent")
 
                 # ------------ username ------------
                 col+=1
                 self.table_view_user.setColumnWidth(col, 250)
-                username_wid=QTableWidgetItem(row_data['uid'])
-                self.table_view_user.setItem(row,col,username_wid)
+                usernameWid=QTableWidgetItem(rowData['uid'])
+                self.table_view_user.setItem(row,col,usernameWid)
 
                 # ------------ name ------------
                 col+=1
                 self.table_view_user.setColumnWidth(col, 470)
-                name_wid=QTableWidgetItem(row_data['firstName']+" "+row_data['lastName'])
-                self.table_view_user.setItem(row,col,name_wid)
+                nameWid=QTableWidgetItem(rowData['firstName']+" "+rowData['lastName'])
+                self.table_view_user.setItem(row,col,nameWid)
 
                 # ------------ status ------------
                 col+=1
@@ -122,34 +122,34 @@ class PharmacyUsersWindow(QWidget):
                 font=QFont()
                 font.setBold(True)
                 font.setPointSize(8)
-                self.btn_status=QPushButton()
-                self.btn_status.setFont(font)
+                self.btnStatus=QPushButton()
+                self.btnStatus.setFont(font)
 
-                if row_data['isActive']=="Y":
-                    self.btn_status.setText(" ACTIVE")
-                    self.btn_status.setIcon(QIcon(os.path.join(self.rootDir, "images", "active.png")))
-                    self.btn_status.setStyleSheet("background-color:#ecf5d9;color:#7fba00;margin:6px 20px 6px 0px;border-radius:6%")
+                if rowData['isActive']=="Y":
+                    self.btnStatus.setText(" ACTIVE")
+                    self.btnStatus.setIcon(QIcon(os.path.join(self.rootDir, "images", "active.png")))
+                    self.btnStatus.setStyleSheet("background-color:#ecf5d9;color:#7fba00;margin:6px 20px 6px 0px;border-radius:6%")
                 else:
-                    self.btn_status.setText(" INACTIVE")
-                    self.btn_status.setIcon(QIcon(os.path.join(self.rootDir, "images", "deactive.png")))
-                    self.btn_status.setStyleSheet("background-color:#fde5de;color:#f25022;margin:6px 20px 6px 0px;border-radius:6%")
-                self.table_view_user.setCellWidget(row, col, self.btn_status)
+                    self.btnStatus.setText(" INACTIVE")
+                    self.btnStatus.setIcon(QIcon(os.path.join(self.rootDir, "images", "deactive.png")))
+                    self.btnStatus.setStyleSheet("background-color:#fde5de;color:#f25022;margin:6px 20px 6px 0px;border-radius:6%")
+                self.table_view_user.setCellWidget(row, col, self.btnStatus)
 
                 # ------------ edit and delete ------------
                 col+=1
-                if row_data['uid']!="admin":
-                    btn_edit = QPushButton()
-                    btn_edit.setIcon(QIcon(QIcon(os.path.join(self.rootDir, "images", "edit_icon.svg"))))
-                    btn_edit.setCursor(Qt.CursorShape.PointingHandCursor)
+                if rowData['uid']!="admin":
+                    btnEdit = QPushButton()
+                    btnEdit.setIcon(QIcon(QIcon(os.path.join(self.rootDir, "images", "edit_icon.svg"))))
+                    btnEdit.setCursor(Qt.CursorShape.PointingHandCursor)
 
-                    btn_dlt = QPushButton()
-                    btn_dlt.setIcon(QIcon(QIcon(os.path.join(self.rootDir, "images", "delete_icon.svg"))))
-                    btn_dlt.setStyleSheet("margin-right:15px;padding-right:5px")
-                    btn_dlt.setCursor(Qt.CursorShape.PointingHandCursor)
+                    btnDlt = QPushButton()
+                    btnDlt.setIcon(QIcon(QIcon(os.path.join(self.rootDir, "images", "delete_icon.svg"))))
+                    btnDlt.setStyleSheet("margin-right:15px;padding-right:5px")
+                    btnDlt.setCursor(Qt.CursorShape.PointingHandCursor)
 
                     layout = QHBoxLayout()
-                    layout.addWidget(btn_edit)
-                    layout.addWidget(btn_dlt)
+                    layout.addWidget(btnEdit)
+                    layout.addWidget(btnDlt)
                     layout.setContentsMargins(0,0,0,0)
                     
                     widget = QWidget()
@@ -157,21 +157,21 @@ class PharmacyUsersWindow(QWidget):
                     widget.setStyleSheet("border-radius:0px;background-color:transparent")
                     self.table_view_user.setCellWidget(row, col, widget)
 
-                    # btn_edit.clicked.connect(partial(self.switchEditWinrxUser, row_data['uid']))
-                    # btn_dlt.clicked.connect(partial(self.delete_user, row_data['uid']))
+                    # btnEdit.clicked.connect(partial(self.switchEditWinrxUser, rowData['uid']))
+                    # btnDlt.clicked.connect(partial(self.deleteUser, rowData['uid']))
         except Exception as e:
             print(e)
 
-    def fetch_all_users(self):
+    def fetchAllUsers(self):
         try:
-            local_cursor = self.local_conn.cursor()
-            local_cursor.execute("SELECT * FROM users where isSoftDlt='N' and uid != 'sys'")
-            data=dictfetchall(local_cursor)
+            localCursor = self.localConn.cursor()
+            localCursor.execute("SELECT * FROM users where isSoftDlt='N' and uid != 'sys'")
+            data=dictfetchall(localCursor)
             self.addDataToUserTable(data)
         except Exception as e:
             print(e)
 
-    def clear_info_messages(self):
+    def clearInfoMessages(self):
             try:
                 self.infoViewUsers.setText("")
                 self.infoViewUsers.setStyleSheet("background:none")
