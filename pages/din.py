a19cec9c7198e6c2fd9a3489f1fb9bc1a07173cb
rexhoney <er.rexhoney@gmail.com>
2025-09-24 17:42:01 +0530

    Add switchViewDins method to DinWindow for enhanced medication display
    - Introduced the switchViewDins method to fetch and display medication data from the din_groups table.
    - Cleared and updated the comboBoxLeftPump with medication options based on pump position.
    - Improved error handling with exception logging for better debugging.

--- pages/din.py
+++ pages/din.py
@@ -2,6 +2,7 @@
 from PyQt6.QtWidgets import QWidget
 from PyQt6 import uic
 import os, pyodbc
+from otherFiles.common import dictfetchall
 
 class DinWindow(QWidget):
     def __init__(self):
@@ -16,3 +17,23 @@ class DinWindow(QWidget):
         rootDir = os.path.dirname(os.path.dirname(__file__))
         ui_path = os.path.join(rootDir, "uiFiles", "din.ui")
         uic.loadUi(ui_path, self)
+        self.switchViewDins()
+
+    def switchViewDins(self):
+        try:
+            self.infoViewDins.setText("")
+            self.infoViewDins.setStyleSheet("background:none;padding:12px;")
+    
+            query = f"select id, medication,pump_position from din_groups"
+            localCursor = self.local_conn.cursor()
+            localCursor.execute(query)
+            data=dictfetchall(localCursor)
+            if data:
+                self.comboBoxLeftPump.clear()
+                for row in data:
+                    self.comboBoxLeftPump.addItem(row['medication'])
+                    if row['pump_position']=='Left':
+                        leftPumpMedication=row['medication']
+                self.comboBoxLeftPump.setCurrentText(leftPumpMedication)
+        except Exception as e:
+            print(e)
