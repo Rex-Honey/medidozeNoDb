46c4f6b0ac32aa7aeb69f778e415f78ede37617d
rexhoney <er.rexhoney@gmail.com>
2025-09-25 18:11:34 +0530

    Add DIN management features to DinWindow
    - Introduced pop-up dialog for adding DIN and strength inputs for medications.
    - Implemented saveDin method to handle DIN addition with validation and database insertion.
    - Added saveMedications method to update medication pump positions based on user selection.
    - Enhanced UI with new input fields and improved styling in din.ui for better user experience.
    - Updated .gitignore to exclude report.py and commits.txt files.

--- pages/din.py
+++ pages/din.py
@@ -1,10 +1,11 @@
-from PyQt6.QtWidgets import QWidget, QTableWidgetItem, QPushButton, QWidget, QHBoxLayout, QMessageBox
+from PyQt6.QtWidgets import QWidget, QTableWidgetItem, QPushButton, QWidget, QHBoxLayout, QMessageBox, QDialog, QVBoxLayout, QLineEdit, QLabel
 from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QIntValidator
 from PyQt6 import uic
 import os, pyodbc
 from otherFiles.common import dictfetchall
 from functools import partial
+from datetime import datetime
 
 
 class DinWindow(QWidget):
@@ -22,6 +23,9 @@ class DinWindow(QWidget):
         uic.loadUi(ui_path, self)
         
         self.comboBoxLeftPump.currentTextChanged.connect(self.medicationChanged)
+        self.btnAddDinLeft.clicked.connect(partial(self.popUpAddDin,pumpPosition="Left"))
+        self.btnAddDinRight.clicked.connect(partial(self.popUpAddDin,pumpPosition="Right"))
+        self.btnSaveMedications.clicked.connect(self.saveMedications)
         self.switchViewDins()
 
 
@@ -133,6 +137,134 @@ class DinWindow(QWidget):
         except Exception as e:
             print(e)
 
+    def popUpAddDin(self,pumpPosition):
+        try:
+            if pumpPosition=="Left":
+                medication=self.comboBoxLeftPump.currentText()
+            else:
+                medication=self.lblRightMedication.text()
+            dinLabel = QLabel("DIN")
+            dinLabel.setStyleSheet("font-size: 14px; font-weight: bold;font-family: Nirmala UI;")
+
+            dinInput = QLineEdit()
+            dinInput.setValidator(QIntValidator())
+            dinInput.setMaxLength(12)
+            dinInput.setPlaceholderText("Enter DIN")
+            dinInput.setStyleSheet("padding:7px;border:1px solid #e1e4e6;font-size:11pt;border-radius:10%") 
+
+            errDin = QLabel("")
+            errDin.setStyleSheet("color:red;")
+
+            strengthLabel = QLabel("Strength")
+            strengthLabel.setStyleSheet("font-size: 14px; font-weight: bold;font-family: Nirmala UI;")
+
+            strengthInput = QLineEdit()
+            strengthInput.setPlaceholderText("Enter Strength")
+            strengthInput.setStyleSheet("padding:7px;border:1px solid #e1e4e6;font-size:11pt;border-radius:10%")
+
+            errStrength = QLabel("")
+            errStrength.setStyleSheet("color: red;")
+
+            saveButton = QPushButton("Add")
+            saveButton.setStyleSheet("border-radius:10%;background:#033E89;font-weight: 700;color:white;padding:9px;font-size:10pt")
+            saveButton.setCursor(Qt.CursorShape.PointingHandCursor)
+            saveButton.clicked.connect(partial(self.saveDin,dinInput,strengthInput,medication,errDin,errStrength))
+
+            self.tempLbl=QLabel("")
+            self.tempLbl.setStyleSheet("padding:2px;")
+
+            dinLayout = QVBoxLayout()
+            dinLayout.setSpacing(2)
+            dinLayout.addWidget(dinLabel)
+            dinLayout.addWidget(dinInput)
+            dinLayout.addWidget(errDin)
+
+            strengthLayout = QVBoxLayout()
+            strengthLayout.setSpacing(2)
+            strengthLayout.addWidget(strengthLabel)
+            strengthLayout.addWidget(strengthInput)
+            strengthLayout.addWidget(errStrength)
+
+            btnLayout = QVBoxLayout()
+            btnLayout.setSpacing(4)
+            btnLayout.addWidget(self.tempLbl)
+            btnLayout.addWidget(saveButton)
+
+            dialogLayout = QVBoxLayout()
+            dialogLayout.setSpacing(10)
+            dialogLayout.addLayout(dinLayout)
+            dialogLayout.addLayout(strengthLayout)
+            dialogLayout.addLayout(btnLayout)
+
+            dialog = QDialog(self)
+            dialog.setWindowTitle(f"Add DIN to {medication}")
+            dialog.setFixedSize(300, 260)
+            dialog.setStyleSheet("background:#FFFFFF;")
+            dialog.setModal(True)
+            dialog.setLayout(dialogLayout)
+            dialog.exec()
+        except Exception as e:
+            print(e)
+
+    def saveDin(self,dinWidget,strengthWidget,medication,errDin,errStrength):
+        try:
+            errDin.setText("")
+            errStrength.setText("")
+
+            din=dinWidget.text()
+            strength=strengthWidget.text()
+            if str(din).strip()=='':
+                errDin.setText("DIN is required")
+                return
+            if str(strength).strip()=='':
+                errStrength.setText("Strength is required")
+                return
+            
+            query = f"select id from din_groups where medication='{medication}'"
+            localCursor = self.local_conn.cursor()
+            localCursor.execute(query)
+            data=dictfetchall(localCursor)
+            if data:
+                dinGroupID=data[0]['id']
+                query = f"insert into din (din_number, strength, din_group_id, createdBy, updatedBy, createdDate, updatedDate) values (?,?,?,?,?,?,?)"
+                localCursor.execute(query, (din,strength,dinGroupID,self.userData['uid'],self.userData['uid'],datetime.now(),datetime.now()))
+                self.local_conn.commit()
+                self.tempLbl.setText("DIN added successfully !!")
+                self.tempLbl.setStyleSheet("background:lightgreen;color:green;padding:2px;border-radius:none")
+                QTimer.singleShot(900, self.clearTempLbl)
+            dinWidget.setText("")
+            strengthWidget.setText("")
+        except Exception as e:
+            print(e)
+
+    def saveMedications(self):
+        try:
+            leftMedication=self.comboBoxLeftPump.currentText()
+            local_cursor = self.local_conn.cursor()
+            if leftMedication=='Metadol':
+                query = f"update din_groups set pump_position='Left' where medication='Metadol'"
+                local_cursor.execute(query)
+
+                query = f"update din_groups set pump_position='Right' where medication='Methadose'"
+                local_cursor.execute(query)
+            else:
+                query = f"update din_groups set pump_position='Left' where medication='Methadose'"
+                local_cursor.execute(query)
+
+                query = f"update din_groups set pump_position='Right' where medication='Metadol'"
+                local_cursor.execute(query)
+
+            self.local_conn.commit()
+            self.infoViewDins.setText("Medications saved successfully")
+            self.infoViewDins.setStyleSheet("background:lightgreen;color:green;padding:12px;border-radius:none")
+            QTimer.singleShot(4000, self.clearinfoViewDins)
+        except Exception as e:
+            print(e)
+
     def clearinfoViewDins(self):
         self.infoViewDins.setText("")
         self.infoViewDins.setStyleSheet("background:none;padding:12px;")
+
+    def clearTempLbl(self):
+        self.tempLbl.setText("")
+        self.tempLbl.setStyleSheet("background:none;padding:2px;")
