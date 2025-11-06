388184a6b1483bfdfc5578849a3e30569d72ef63
rexhoney <er.rexhoney@gmail.com>
2025-11-06 15:58:38 +0530

    Implement dispense dose amount retrieval in DashboardWindow
    - Added a new method, getDispenseDoseAmount, to calculate and display the total doses dispensed by left and right pumps for the current date.
    - Enhanced error handling in existing methods to provide more informative error messages.
    - Refactored import statements to streamline the initialization process in the DashboardWindow class.

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -5,6 +5,7 @@ import os, pyodbc
 import serial.tools.list_ports
 from PyQt6.QtCore import Qt, QThread, pyqtSignal, QObject
 from otherFiles.common import sendPcbCommand
+from datetime import datetime
 
 STYLE_READY = (
     "background:#7FBA00;"
@@ -27,12 +28,7 @@ STYLE_NOT_READY = (
 class DashboardWindow(QWidget):
     def __init__(self):
         super().__init__()
-        from otherFiles.config import config, userData, localConn, updatePcbComPort, leftPumpCalibrated, rightPumpCalibrated
-        if config is None or localConn is None:
-            print("Configuration not properly initialized. Please restart the application.")
-            return
-        self.config = config
-        self.userData = userData
+        from otherFiles.config import localConn, updatePcbComPort, leftPumpCalibrated, rightPumpCalibrated
         self.localConn = localConn
         self.updatePcbComPort = updatePcbComPort
         self.leftPumpCalibrated = leftPumpCalibrated
@@ -50,11 +46,12 @@ class DashboardWindow(QWidget):
 
     def loadInitialData(self):
         try:
+            self.getDispenseDoseAmount()
             self.listUsbPorts()
             self.workerThread.start()
             self.workerThread.started.connect(self.worker.checkPumpStatusWorker)
         except Exception as e:
-            print(e)
+            print("load_Initial_Data error:",e)
 
     def listUsbPorts(self):
         try:
@@ -83,7 +80,7 @@ class DashboardWindow(QWidget):
             if not pcbConnected:
                 print("PCB not connected")
         except Exception as e:
-            print(e)
+            print("list_Usb_Ports error:",e)
 
     def updatePumpStatus(self,status):
         try:
@@ -122,7 +119,53 @@ class DashboardWindow(QWidget):
             except (TypeError, RuntimeError):
                 pass
         except Exception as e:
-            print(e)
+            print("update_Pump_Status error:",e)
+
+    def getDispenseDoseAmount(self):
+        try:
+            currentDate=datetime.now()
+            formattedCurrentDate = currentDate.strftime('%Y-%m-%d %H:%M:%S')
+            localCursor = self.localConn.cursor()
+            localCursor.execute(f"""SELECT 
+                COALESCE(ROUND(( 
+                    SELECT SUM(dl.dlDose) 
+                    FROM dispenseLogs dl 
+                    JOIN din_groups dg ON dg.id = dl.dlMedicationID 
+                    WHERE dg.pump_position = 'Left' 
+                    AND CONVERT(date,dl.createdDate) = '{formattedCurrentDate}'
+                ), 2), 0) + 
+                COALESCE(ROUND(( 
+                    SELECT SUM(idl.idlDose) 
+                    FROM instantDoseLogs idl 
+                    JOIN din_groups dg ON dg.id = idl.idlMedicationID 
+                    WHERE dg.pump_position = 'Left' 
+                    AND CONVERT(date,idl.createdDate) = '{formattedCurrentDate}'
+                ), 2), 0) AS total_left_pump, 
+                COALESCE(ROUND(( 
+                    SELECT SUM(dl.dlDose) 
+                    FROM dispenseLogs dl 
+                    JOIN din_groups dg ON dg.id = dl.dlMedicationID 
+                    WHERE dg.pump_position = 'Right' 
+                    AND CONVERT(date,dl.createdDate) = '{formattedCurrentDate}'
+                ), 2), 0) + 
+                COALESCE(ROUND(( 
+                    SELECT SUM(idl.idlDose) 
+                    FROM instantDoseLogs idl 
+                    JOIN din_groups dg ON dg.id = idl.idlMedicationID 
+                    WHERE dg.pump_position = 'Right' 
+                    AND CONVERT(date,idl.createdDate) = '{formattedCurrentDate}'
+                ), 2), 0) AS total_right_pump""")
+            dispenseTotal = localCursor.fetchall()[0]
+            totalLeft=dispenseTotal[0]
+            totalRight=dispenseTotal[1]
+            if totalLeft==int(totalLeft):
+                totalLeft=int(totalLeft)
+            self.lblFilledLeft.setText(str(totalLeft))
+            if totalRight==int(totalRight):
+                totalRight=int(totalRight)
+            self.lblFilledRight.setText(str(totalRight))
+        except Exception as e:
+            print("get_Dispense_Dose_Amount error:",e)
 
 class Worker(QObject):
     chkPumpStatus=pyqtSignal(str)
@@ -138,5 +181,5 @@ class Worker(QObject):
             else:
                 self.chkPumpStatus.emit('error')
         except Exception as e:
-            print("check_Pump_Status_Worker error",e)
+            print("check_Pump_Status_Worker error:",e)
             self.chkPumpStatus.emit('error')
